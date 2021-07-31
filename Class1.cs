9d4e9f357a3bd3583c0b296092ee81030c42caae
Mod <rotichmanuela@gmail.com>
2021-07-31 10:23:50 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+
+namespace Exercise02
+{
+    public class Class1
+    {
+        //Dynamic definition
+        public static string IntegerValue;
+        public static string DecribedValue;
+
+        //Strong type definitions
+        public static string Strong_Input_value;
+        public static string One_Million_Val = "1000000";
+        public static string One_million = "One million";
+        public static string Two_million_Val = "2000000";
+        public static string Two_million = "Two million";
+
+        public void DescribeValue()
+        {
+            //Describe integer
+            BigInteger _describeValue = BigInteger.Pow(Convert.ToInt64(IntegerValue), 3);
+            DecribedValue = _describeValue.ToString();
+        }
+    }
+}
